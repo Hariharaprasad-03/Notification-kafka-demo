a485087fd5b891279efd064fa4fd01885a500fca
Hariharaprasad-03 <hariharaprasad03@gmail.com>
2026-06-29 22:30:42 +0530

    message demo like system

---
+++ src/main/java/com/zsgs/notification_kafka_demo/NotificationKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zsgs.notification_kafka_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationKafkaDemoApplication.class, args);
+	}
+
+}
